0ef1c3838a2d4d8f39c5511258822646ce4272db
zajan <zajan91@naver.com>
2019-09-17 11:47:05 +0900

    first commit

---
+++ easylog.go
@@ -0,0 +1,203 @@
+package easylog
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sync"
+	"time"
+)
+
+const (
+	defaultLevel    = DEBUG
+	defaultMaxSize  = 100
+	defaultFilePath = "./"
+)
+
+var (
+	logInit  bool = false
+	level    LogLevel
+	logger   *log.Logger
+	fp       *os.File
+	maxSize  int
+	filePath string
+	fileName string
+	mutex    sync.Mutex
+)
+
+func init() {
+	level = defaultLevel
+	maxSize = defaultMaxSize
+	filePath = defaultFilePath
+	fileName = fmt.Sprintf("%s.log", os.Args[0])
+}
+
+func Init(options ...func()) {
+
+	if logInit {
+		return
+	}
+
+	for _, f := range options {
+		f()
+	}
+
+	checkFile()
+
+	if logger == nil {
+		panic("logger is nil pointer")
+	}
+
+	logInit = true
+}
+
+func Debug(msg string, v ...interface{}) {
+	checkFile()
+
+	if level <= DEBUG {
+		fmtMsg := format(DEBUG, msg)
+		logger.Printf(fmtMsg, v...)
+	}
+}
+
+func Info(msg string, v ...interface{}) {
+	checkFile()
+
+	if level <= INFO {
+		fmtMsg := format(INFO, msg)
+		logger.Printf(fmtMsg, v...)
+	}
+}
+
+func Warn(msg string, v ...interface{}) {
+	checkFile()
+
+	if level <= WARN {
+		fmtMsg := format(WARN, msg)
+		logger.Printf(fmtMsg, v...)
+	}
+}
+
+func Error(msg string, v ...interface{}) {
+	checkFile()
+
+	if level <= ERROR {
+		fmtMsg := format(ERROR, msg)
+		logger.Printf(fmtMsg, v...)
+	}
+}
+
+func Fatal(msg string, v ...interface{}) {
+	checkFile()
+
+	if level <= FATAL {
+		fmtMsg := format(FATAL, msg)
+		logger.Fatalf(fmtMsg, v...)
+	}
+}
+
+func checkFile() {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if fp == nil {
+		if err := openFile(filePath, fileName); err != nil {
+			panic(err)
+		}
+	}
+
+	if isFileMax(fp) {
+		closeFile()
+
+		if err := renameFile(); err != nil {
+			panic(err)
+		}
+
+		if err := openFile(filePath, fileName); err != nil {
+			panic(err)
+		}
+
+		setNewLogger(fp)
+	}
+
+	if logger == nil {
+		setNewLogger(fp)
+	}
+}
+
+func renameFile() error {
+	old := fmt.Sprintf("%s/%s", filePath, fileName)
+	new := fmt.Sprintf("%s/%s.bak.%s", filePath, fileName, time.Now().Format("20060102150405"))
+
+	if err := os.Rename(old, new); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setNewLogger(fp *os.File) {
+	logger = log.New(fp, "", 0)
+}
+
+func closeFile() {
+	if fp == nil {
+		return
+	}
+
+	fp.Close()
+
+	fp = nil
+}
+
+func isFileMax(fp *os.File) bool {
+	info, err := fp.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	if info.Size() >= getMaxByte() {
+		return true
+	}
+
+	return false
+}
+
+func getMaxByte() int64 {
+	return int64(maxSize) * 1024 * 1024 // MB to Byte
+}
+
+func openFile(path string, name string) error {
+	var err error
+
+	fp, err = os.OpenFile(fmt.Sprintf("%s/%s", path, name), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func SetLevel(lv LogLevel) func() {
+	return func() {
+		level = lv
+	}
+}
+
+func SetMaxSize(size int) func() {
+	return func() {
+		maxSize = size
+	}
+}
+
+func SetFilePath(path string) func() {
+	return func() {
+		filePath = path
+	}
+}
+
+func SetFileName(name string) func() {
+	return func() {
+		fileName = name
+	}
+}
---
+++ examples/example1.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"sync"
+
+	log "github.com/zajann/easylog"
+)
+
+func main() {
+
+	wg := new(sync.WaitGroup)
+
+	log.Init(
+		log.SetLevel(log.DEBUG),
+		log.SetFilePath("./"),
+		log.SetFileName("test.log"),
+		log.SetMaxSize(1),
+	)
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			for j := 0; j < 100; j++ {
+				log.Info("EXAMPLE: %d", j)
+			}
+		}()
+	}
+
+	wg.Wait()
+}
