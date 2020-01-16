6d6bd6878115e14351b66f8ece15e340b3cd3a6d
zajann <zajan.ksk@gmail.com>
2020-01-16 23:08:34 +0900

    fix

---
+++ examples/example1.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"sync"
+
+	log "github.com/zajann/easylog"
+)
+
+func main() {
+
+	wg := new(sync.WaitGroup)
+
+	if err := log.Init(
+		log.SetLevel(log.DEBUG),
+		log.SetFilePath("./"),
+		log.SetFileName("test.log"),
+		log.SetMaxSize(1),
+	); err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			for j := 0; j < 100; j++ {
+				log.Info("EXAMPLE: %d", j)
+			}
+		}()
+	}
+
+	wg.Wait()
+}
